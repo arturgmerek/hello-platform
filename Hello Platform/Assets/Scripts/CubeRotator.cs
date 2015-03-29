f679d6a3b6cfdbcc78e36c3ce0f2640fa0b05192
Litwiniak <evilofpeople@gmail.com>
2015-03-29 16:23:17 +0200

    basicassetspacks

---
+++ Hello Platform/Assets/Scripts/CubeRotator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+public class CubeRotator : MonoBehaviour {
+	public float rotX = 0.1f;
+
+	// Use this for initialization
+
+	void Start () {
+	
+
+	}
+
+	
+
+	// Update is called once per frame
+
+	void Update () {this.transform.Rotate(rotX, 0.3f, 0.1f);
+		rotX += 0.01f;
+	this.transform.position = new Vector3(5.0f * rotX, 0.0f, 0.0f);
+	 	if (this.transform.position.x > 5.0f) {
+			this.transform.position = new Vector3 (0.0f, 0.0f, 0.0f);
+		} else {this.transform.position = new Vector3(5.0f * rotX, 0.0f, 0.0f);
+		}
+
+	}
+
+}
