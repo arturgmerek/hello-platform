fd95db7c9a26dcebd0d429b71462f3c9095c3f73
Artur Gmerek <arturgmerek@gmail.com>
2015-03-29 20:30:34 +0200

    ball movement

---
+++ Hello Platform/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	float inputH = 0.0f;
+	float inputV = 0.0f;
+	
+	void Update () 
+	{
+		Rigidbody body = this.GetComponent<Rigidbody>();
+
+		inputH = Input.GetAxis ("Horizontal");
+		if (inputH != 0.0f) {
+			body.AddForce(inputH * 5, 0.0f, 0.0f);
+		}
+
+		inputV = Input.GetAxis ("Vertical");
+		if (inputV > 0.0f) {
+			body.AddForce(0.0f, inputV * 15, 0.0f);
+		}
+	}
+}
