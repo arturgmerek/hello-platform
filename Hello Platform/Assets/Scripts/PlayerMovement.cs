670cc7bf62d2c8d0e05144c7ebc365de9c36e98d
Litwiniak <evilofpeople@gmail.com>
2015-04-01 21:49:20 +0200

    newplatforms

--- Hello Platform/Assets/Scripts/PlayerMovement.cs
+++ Hello Platform/Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
+	public GameObject other;
+	public Camera cam = other.GetComponent<Camera>();
 	float inputH = 0.0f;
 	public float inputHfactor= 1.0f;
 	float inputV = 0.0f;
 	public float inputVfactor = 1.0f;
 
 	void Update () 
-	{
+		
+		{
 		Rigidbody body = this.GetComponent<Rigidbody>();
 
 		inputH = Input.GetAxis ("Horizontal");
@@ -20,6 +22,10 @@ public class PlayerMovement : MonoBehaviour {
 		inputV = Input.GetAxis ("Vertical");
 		if (inputV > 0.0f) {
 			body.AddForce(0.0f, inputV * 15, 0.0f);
+			{
+				cam.fieldOfView = 45;	}
+			}
 		}
 	}
-}
+
+
