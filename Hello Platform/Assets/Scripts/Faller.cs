fbaf9e35792812ab3e18b400b95016b4d5975ec1
Litwiniak <evilofpeople@gmail.com>
2015-05-12 19:21:59 +0200

    masz

---
+++ Hello Platform/Assets/Scripts/Faller.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Faller : MonoBehaviour {
+
+	void OnCollisionEnter(){
+		Rigidbody body = this.GetComponent<Rigidbody> ();
+
+		body.useGravity = true;
+	}
+}
