3c2d21914a6cb3e0f1dafcc793ba33e358fa7fff
Litwiniak <evilofpeople@gmail.com>
2015-05-15 17:03:12 +0200

    mojewypociny

--- Hello Platform/Assets/Scripts/GeldDestroyer.cs
+++ Hello Platform/Assets/Scripts/GeldDestroyer.cs
@@ -3,6 +3,16 @@ using System.Collections;
 
 public class GeldDestroyer : MonoBehaviour {
 	public int score;
+	public int lifes;
+	void Start() {
+		score = 0;
+		lifes = 5;
+	}
+	void OnGUI() {
+		// Make a background box
+		GUI.Box(new Rect(10,10,50,50), "Score\n" + score.ToString());
+		GUI.Box (new Rect (60, 10, 50, 50), "Lifes\n" + lifes.ToString());
 
+	}
 		
 }
