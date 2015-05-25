7359bcb5309284d3ff22670214acee08af70e165
Litwiniak <evilofpeople@gmail.com>
2015-05-25 22:31:45 +0200

    problemy

--- Hello Platform/Assets/Scripts/GeldDestroyer.cs
+++ Hello Platform/Assets/Scripts/GeldDestroyer.cs
@@ -20,7 +20,7 @@ public class GeldDestroyer : MonoBehaviour {
 		GUI.Box(new Rect(10,10,50,50), "Score\n" + score.ToString());
 		GUI.Box (new Rect (60, 10, 50, 50), "Lifes\n" + lifes.ToString());
 		if (PlayerPrefs.GetString("CurrentLevel") == "3") {
-			GUI.Box (new Rect (900, 300, 300, 100), "Game Over");
+			GUI.Box (new Rect (800, 300, 300, 100), "Game Over");
 			GUI.Button (new Rect (900, 400, 200, 50), "Continue"); 
 				Application.LoadLevel ("first");
 			Debug.Log ("sexujesie");
@@ -31,9 +31,9 @@ public class GeldDestroyer : MonoBehaviour {
 			this.lifes --;
 			}
 
-		if(lifes < 0){
+		if(lifes < -1){
 			Application.LoadLevel ("Third");
-
+			PlayerPrefs.SetString ("CurrentLevel", "3");
 		}
 	}
 
